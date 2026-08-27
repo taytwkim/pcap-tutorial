21d4cc7c11fcb806d032161c7e801d4db49b34a8
Tay Kim <tay.tw.kim@gmail.com>
2026-08-26 22:11:20 -0400

    hello world

---
+++ hello/README.md
@@ -0,0 +1,19 @@
+# PcapPlusPlus Tutorial - Writing a simple app
+
+[Write a simple application](https://pcapplusplus.github.io/docs/tutorials/intro).
+
+1. Install PcapPlusPlus.
+
+2. Build using CMake from project root.
+
+```shell
+cmake -S hello -B build/hello
+cmake --build build/hello
+```
+
+3. Change to the `hello/` directory and run the executable:
+
+```shell
+cd hello
+./Tutorial-HelloWorld
+```
---
+++ hello/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <IPv4Layer.h>
+#include <Packet.h>
+#include <PcapFileDevice.h>
+
+int main (int argc, char* argv []) {
+	pcpp::PcapFileReaderDevice reader("1_packet.pcap");
+
+	// open a pcap file
+	if (!reader.open()) {
+		std::cerr << "Error opening the pcap file" << std::endl;
+		return 1;
+	}
+
+	// read the first and only packet in the file.
+	pcpp::RawPacket rawPacket;
+
+	if (!reader.getNextPacket(rawPacket)) {
+		std::cerr << "Couldn't read the first packet in the file." << std::endl;
+		return 1;
+	}
+
+	// parse the raw packet into a parsed packet
+	pcpp::Packet parsedPacket(&rawPacket);
+
+	// verify the packet is IPv4
+	if (parsedPacket.isPacketOfType(pcpp::IPv4)) {
+		pcpp::IPv4Address srcIP = parsedPacket.getLayerOfType<pcpp::IPv4Layer>()->getSrcIPv4Address();
+		pcpp::IPv4Address destIP = parsedPacket.getLayerOfType<pcpp::IPv4Layer>()->getDstIPv4Address();
+
+		std::cout << "Source IP is '" << srcIP << "';"
+			  		<< "Dest IP is '" << destIP << "'" << std::endl;
+	}
+
+	reader.close();
+
+	return 0;
+}
